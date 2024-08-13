c245a9801fcb3175951a18ac4f643bfd9cbc6c22
mayank2130 <mayankthakur1712@gmail.com>
2024-08-13 20:43:42 +0530

    configs change & database

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,4 @@
-import "./globals.css";
-import "@repo/ui/styles.css";
+import "@repo/tailwind-config/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,10 +1,1 @@
-import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
-
-const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
-  presets: [sharedConfig],
-};
-
-export default config;
+module.exports = require("@repo/tailwind-config/tailwind.config");
