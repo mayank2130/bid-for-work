95c2dea35ae430af69614e26b54c47fb8917f1ce
mayank2130 <mayankthakur1712@gmail.com>
2024-08-29 20:17:05 +0530

    footer section data

--- packages/ui/lib/feature.ts
+++ packages/ui/lib/feature.ts
@@ -52,12 +52,88 @@ export const popularServices = [
     color: "bg-green-900",
     image:
       "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156476/software-development.png",
-  }
+  },
 ];
 
 export const features = [
-  { icon: "👥", text: "Access a pool of top talent across 700 categories" },
+  { icon: "👥", text: "Access a pool of top talent across the Globe" },
   { icon: "🔄", text: "Enjoy a simple, easy-to-use matching experience" },
   { icon: "⚡", text: "Get quality work done quickly and within budget" },
   { icon: "💰", text: "Only pay when you're happy" },
+];
+
+export const footerSections = [
+  {
+    title: "Categories",
+    items: [
+      "Graphics & Design",
+      "Digital Marketing",
+      "Writing & Translation",
+      "Video & Animation",
+      "Music & Audio",
+      "Fiverr Logo Maker",
+      "Programming & Tech",
+      "Data",
+      "Business",
+      "Personal Growth & Hobbies",
+      "Photography",
+      "Finance",
+      "End-to-End Projects",
+      "Sitemap",
+    ],
+  },
+  {
+    title: "About",
+    items: [
+      "Careers",
+      "Press & News",
+      "Partnerships",
+      "Privacy Policy",
+      "Terms of Service",
+      "Intellectual Property Claims",
+      "Investor Relations",
+    ],
+  },
+  {
+    title: "Support and Education",
+    items: [
+      "Help & Support",
+      "Trust & Safety",
+      "Quality Guide",
+      "Selling on Fiverr",
+      "Buying on Fiverr",
+      "Fiverr Guides",
+      "Learn",
+      "Online Courses",
+    ],
+  },
+  {
+    title: "Community",
+    items: [
+      "Customer Success Stories",
+      "Community Hub",
+      "Forum",
+      "Events",
+      "Blog",
+      "Creators",
+      "Affiliates",
+      "Podcast",
+      "Invite a Friend",
+      "Become a Seller",
+      "Community Standards",
+    ],
+  },
+  {
+    title: "Business Solutions",
+    items: [
+      "About Business Solutions",
+      "Fiverr Pro",
+      "Fiverr Certified",
+      "Become an Agency",
+      "Fiverr Enterprise",
+      "ClearVoice Content Marketing",
+      "Working Not Working",
+      "Contact Sales",
+    ],
+  },
 ];
