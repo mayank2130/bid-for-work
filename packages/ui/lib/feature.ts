842f9970592263a4400eba947f2726dd8c7f1a99
mayank2130 <mayankthakur1712@gmail.com>
2024-08-29 16:53:47 +0530

    popular features

---
+++ packages/ui/lib/feature.ts
@@ -0,0 +1,63 @@
+export const categories = [
+  { name: "Programming & Tech", icon: "💻" },
+  { name: "Graphics & Design", icon: "🎨" },
+  { name: "Digital Marketing", icon: "📱" },
+  { name: "Writing & Translation", icon: "✍️" },
+  { name: "Video & Animation", icon: "🎥" },
+  { name: "AI Services", icon: "🤖" },
+  { name: "Music & Audio", icon: "🎵" },
+  { name: "Business", icon: "💼" },
+  { name: "Consulting", icon: "🤝" },
+];
+
+export const popularServices = [
+  {
+    name: "Video Editing",
+    color: "bg-green-900",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156494/video-editing.png",
+  },
+  {
+    name: "Logo Design",
+    color: "bg-orange-400",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156494/logo-design.png",
+  },
+  {
+    name: "SEO",
+    color: "bg-green-800",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156488/seo.png",
+  },
+  {
+    name: "Architecture & Interior Design",
+    color: "bg-pink-700",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156473/architecture-design.png",
+  },
+  {
+    name: "Social Media Marketing",
+    color: "bg-yellow-600",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156476/social-media-marketing.png",
+  },
+  {
+    name: "Voice Over",
+    color: "bg-amber-700",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156479/voice-over.png",
+  },
+  {
+    name: "Software Development",
+    color: "bg-green-900",
+    image:
+      "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_188,dpr_1.0/v1/attachments/generic_asset/asset/798403f5b92b1b5af997acc704a3d21c-1702465156476/software-development.png",
+  }
+];
+
+export const features = [
+  { icon: "👥", text: "Access a pool of top talent across 700 categories" },
+  { icon: "🔄", text: "Enjoy a simple, easy-to-use matching experience" },
+  { icon: "⚡", text: "Get quality work done quickly and within budget" },
+  { icon: "💰", text: "Only pay when you're happy" },
+];
