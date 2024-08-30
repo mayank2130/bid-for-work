10303f9c2c4d86df864e4f73fd6dec5c3c74bdf1
mayank2130 <mayankthakur1712@gmail.com>
2024-08-30 13:52:08 +0530

    bids page bg color

--- apps/web/src/app/bids/page.tsx
+++ apps/web/src/app/bids/page.tsx
@@ -5,11 +5,10 @@ import NavBar from "@repo/ui/components/navbar";
 const Page = async () => {
   const session = await auth();
   return (
-    <div className="overflow-y-auto w-full flex-1 px-40 mt-8">
-      <NavBar session={session} />
-
-      <Bids />
-    </div>
+    <div className="min-h-screen pt-8 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
+    <NavBar session={session} />
+    <Bids />
+  </div>
   );
 };
 
